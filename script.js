2a12aa14ee607a2d0e28d743372d90277a4e8cdf
Chao-Freeman <124365937+Chao-Freeman@users.noreply.github.com>
2024-11-06 18:32:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-addEventListener("onload", function(){
-      let elever=documents.getElementById("FTCDashVer");
-      function showErrorMsg(){
-          elever.innerHTML = "Failed to get latest version, please visit <a href=\"https://acmerobotics.github.io/ftc-dashboard/gettingstarted\"><strong>this</strong></a> site to get latest version."
-      }
-      fetch("https://acmerobotics.github.io/ftc-dashboard/gettingstarted").then(function success(result){
-          let r=result.text().match(/(?<=implementation 'com.acmerobotics.dashboard:dashboard:).+?(?=')/);
-          if(r!=null){
-              elever.innerText = r[0];
-          }
-          showErrorMsg();
-      }, function(result){
-          showErrorMsg();
-          console.error(result);
-      })
-  });
+addEventListener("load", function(){
+let elever=documents.getElementById("FTCDashVer");
+function showErrorMsg(){
+    elever.innerHTML = "Failed to get latest version, please visit <a href=\"https://acmerobotics.github.io/ftc-dashboard/gettingstarted\"><strong>this</strong></a> site to get latest version."
+}
+fetch("https://acmerobotics.github.io/ftc-dashboard/gettingstarted").then(function success(result){
+    let r=result.text().match(/(?<=implementation 'com.acmerobotics.dashboard:dashboard:).+?(?=')/);
+    if(r!=null){
+        elever.innerText = r[0];
+    }
+    showErrorMsg();
+}, function(result){
+    showErrorMsg();
+    console.error(result);
+})
+});
